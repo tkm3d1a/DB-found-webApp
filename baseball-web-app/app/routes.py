b433bc34bfc833ccfe5e32fa8969e3cb2f101792
Pawan Bhandari <zephyrpawan@gmail.com>
2022-11-20 17:07:19 -0600

    login implementation in progress

--- baseball-web-app/app/routes.py
+++ baseball-web-app/app/routes.py
@@ -1,6 +1,7 @@
 import sys
 import logging
-from flask import render_template, request, flash, redirect, url_for
+import pymysql
+from flask import render_template, request, flash, redirect, url_for, session
 from app import app
 from app.forms import LoginForm, SearchForm
 from app.orm import Analysis
@@ -39,3 +40,39 @@ def search():
         row.setRC27()
     return render_template('search.html', title='Results', form=search_form, results=results)
   return render_template('search.html', title='Search', form=search_form)
+
+@app.route('/signin', methods =['GET', 'POST'])
+def signin():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        con = pymysql.connect(host='localhost', user='root', password='7998', db='baseball', cursorclass=pymysql.cursors.DictCursor)
+        cursor = con.cursor()
+        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password))
+        accounts = cursor.fetchall()
+        account = cursor.fetchone()
+
+        for row in accounts:
+            print(row)
+
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            msg = 'Logged in successfully !'
+            return render_template('welcome.html', msg = msg)
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('signin.html', msg = msg)
+
+@app.route('/register')
+def register():
+    return "You shall register...!"
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
